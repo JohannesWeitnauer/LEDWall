a5694027fd7cdb46742937bd77a1e71f43a6253d
Johannes <johannesweitnauer0204@gmail.com>
2016-10-22 19:53:50 +0200

    diodecoord.ino
    Die function für die Koordinaten, es fehlt das "y-tausch-script"

---
+++ diodecoordassign.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+void coordlight(int xcoord, int ycoord, int rgb1, int rgb2, int rgb3)
+{
+	
+}
+//to use the coordlight function, type: coordlight(xcoordinate, ycoordinate, rgbvalue 1, rgbvalue 2, rgbvalue 3);
+void setup()
+{
+	//setup code here
+}
+void loop()
+{
+	//looped code here
+}
